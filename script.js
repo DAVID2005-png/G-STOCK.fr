b5fc09c56e7375ba1d504af5ce3239ec30902e16
SHILLA <davidmakindu88@gmail.com>
2025-06-23 02:14:14 +0200

    Add files via upload
    Signed-off-by: SHILLA <davidmakindu88@gmail.com>

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-new Swiper('.card-wrapper', {
-  loop: true,
-  spaceBetween: 30,
+const fas=document.querySelector('#fas')
+const homeScreen=document.getElementById("homeScreen")
+fas.addEventListener('click', ()=>{
+  homeScreen.classList.toggle('open')
+})
 
-  // pagination bullets
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-//   responsive breakpoints
-  breakpoints: {
-    0: {
-        slidesPerView: 1
-    },
-    768: {
-        slidesPerView: 2
-    },
-    1024: {
-        slidesPerView: 3
-    },
-  }
-});
+const fas1=document.querySelector('#fas1')
+fas1.addEventListener('click', ()=>{
+  homeScreen.classList.toggle('open')
+})
